5533418b5f270d4d742fba86f582aa33bf956d59
emran <shuvoorba28@gmail.com>
2023-09-14 20:45:09 +0600

    initial set, added taiwind, removed extra css and texts images etc

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 className='text-5xl font-bold hover:bg-red-500'>Course registration</h1>
+    </>
+  )
+}
+
+export default App
